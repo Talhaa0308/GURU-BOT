6b2c958ef2431179baa89c7480a5294d32cfe9e7
Muhammad Talha <155046833+Talhaa0308@users.noreply.github.com>
2023-12-29 09:22:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923478281814', 'Online Tricks', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
